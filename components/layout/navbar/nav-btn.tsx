e4ff3124f0fe3e20da5cd5a79906dcd2b5b86230
KodeWithKK <kodewithkk@gmail.com>
2024-11-05 20:03:59 +0530

    fixed User Options uneven positions

--- components/layout/navbar/nav-btn.tsx
+++ components/layout/navbar/nav-btn.tsx
@@ -11,7 +11,7 @@ function NavButton({ Icon, onClick, children }: Readonly<NavButtonProps>) {
       className="flex items-center gap-[5px] rounded-lg py-[7px] pl-[3px] pr-[8px] hover:bg-gray-100 lg:px-[10px]"
       onClick={onClick}
     >
-      <Icon className="h-[24px] text-gray-950 max-md:h-[28px]" />
+      <Icon className="h-[28px] text-gray-950 md:h-[24px]" />
       <div className="hidden text-gray-950 decoration-dashed lg:block">
         {children}
       </div>
